b78eb0eb78381fb2aa0b9e9d985ef9704ed69951
Bushra <bushraiqbalch786@gmail.com>
2025-10-12 14:50:31 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+# snake water gun game 
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+if( a ==1 & b==1):
+    print("Draw")
+elif (a ==0 & b == 0):
+    print("win")
+else:
+    print("lose")
