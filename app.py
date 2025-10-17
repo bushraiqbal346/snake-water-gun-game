653dceb91f1b178354bfbb019af0e8cdbe3d1b25
Bushra <bushraiqbalch786@gmail.com>
2025-10-17 16:32:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,3 +23,30 @@ def hello():
    print("Hello worlld")
 
 hello()
+
+
+# slove with logics 
+
+import random 
+def check(comp,user):
+    if comp ==user:
+        return 0
+    if comp == 0 and user == 1:
+        return -1
+    if comp == 1 and user == 2:
+        return -1
+    if comp == 2 and user == 2:
+        return -1
+    return 1 
+comp = random.randint(0,2)    
+user = int(input("o for snake, 1 for water and 2 for gun:"))
+score = check(comp , user)
+
+print("you",user)
+print("comp",comp)
+if score == 0:
+    print("its a draw")
+elif score == -1:
+    print("you lose")
+else:
+    ("you won")  
