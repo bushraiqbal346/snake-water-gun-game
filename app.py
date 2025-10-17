2da1d2c58f8d9da166278b3db62c42a41ae63557
Bushra <bushraiqbalch786@gmail.com>
2025-10-17 09:22:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,19 @@ elif (a ==0 & b == 0):
     print("win")
 else:
     print("lose")
+
+
+
+# Decorators in python 
+def greet(fx):
+   def mfx():
+     print("good morning")
+     fx()
+     print("Thanks for using this function")
+   return mfx  
+
+@greet        
+def hello():
+   print("Hello worlld")
+
+hello()
